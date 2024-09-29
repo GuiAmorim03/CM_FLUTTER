f82ba161d65198b4104a2a49a103e737156b7484
Guilherme Amorim <guilhermeamorim@ua.pt>
2024-09-29 23:12:33 +0100

    home screen maps and markers

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 final List<Map<String, dynamic>> locais = [
-  {"id":1, "nome": "Big Ben", "distance": "1.3km", "king":"shrek"},
-  {"id":2, "nome": "Eiffel Tower", "distance": "1.8km", "king":"shrek"},
-  {"id":3, "nome": "Berlin Wall", "distance": "450m", "king":"shrek"},
+  {"id":1, "nome": "Big Ben", "distance": 1300, "coord":{"lat": 51.510357, "lng": -0.116773}, "visited": false},
+  {"id":2, "nome": "Eiffel Tower", "distance": 1800, "coord":{"lat": 48.858093, "lng": 2.294694}, "visited": false},
+  {"id":3, "nome": "Berlin Wall", "distance": 450, "coord":{"lat": 52.535152, "lng": 13.390206}, "visited": true},
+  {"id":4, "nome": "Sagrada Familia", "distance": 230, "coord":{"lat": 41.403706, "lng": 2.173504}, "visited": false},
+  {"id":5, "nome": "Colosseum", "distance": 800, "coord":{"lat": 41.890210, "lng": 12.492231}, "visited": true},
+  {"id":6, "nome": "Statue of Liberty", "distance": 1200, "coord":{"lat": 40.689247, "lng": -74.044502}, "visited": true},
+  {"id":7, "nome": "Taj Mahal", "distance": 1500, "coord":{"lat": 27.175015, "lng": 78.042155}, "visited": true},
+  {"id":8, "nome": "Great Wall of China", "distance": 2000, "coord":{"lat": 40.431908, "lng": 116.570374}, "visited": false},
+  {"id":9, "nome": "Machu Picchu", "distance": 3000, "coord":{"lat": -13.163141, "lng": -72.545894}, "visited": true},
+  {"id":10, "nome": "Sydney Opera House", "distance": 4000, "coord":{"lat": -33.856784, "lng": 151.215296}, "visited": true},
+  {"id":11, "nome": "Petra", "distance": 5000, "coord":{"lat": 30.328460, "lng": 35.441397}, "visited": false},
+  {"id":12, "nome": "Chichen Itza", "distance": 6000, "coord":{"lat": 20.684289, "lng": -88.567781}, "visited": false},
+  {"id":13, "nome": "Christ the Redeemer", "distance": 7000, "coord":{"lat": -22.951916, "lng": -43.210487}, "visited": false},
+  {"id":14, "nome": "Machu Picchu", "distance": 8000, "coord":{"lat": -13.163141, "lng": -72.545894}, "visited": false},
+  {"id":15, "nome": "Sydney Opera House", "distance": 9000, "coord":{"lat": -33.856784, "lng": 151.215296}, "visited": false},
+  {"id":16, "nome": "Petra", "distance": 10000, "coord":{"lat": 30.328460, "lng": 35.441397}, "visited": false},
 ];
+
+final List<Map<String, dynamic>> locaisTop5Nearby = (locais..sort((a, b) => a["distance"].compareTo(b["distance"]))).sublist(0, 5);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -23,6 +40,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: DefaultTabController(
+        initialIndex: 1,
         length: 3,
         child: Scaffold(
           appBar: AppBar(
@@ -65,47 +83,96 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: locais.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(
-              Icons.place,
-              color: Colors.green.shade700
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: locaisTop5Nearby.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(
+                    Icons.place,
+                    color: Colors.green.shade700,
+                  ),
+                  title: Text(
+                    locais[index]["nome"],
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: Text(
+                    "${locaisTop5Nearby[index]["distance"]}m",
+                    style: TextStyle(
+                      color: Colors.green.shade700,
+                    ),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
+                    side: BorderSide(
+                      color: Colors.green.shade700,
+                      width: .25,
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => PoiScreen(
+                          poiID: locais[index]["id"],
+                        ),
+                      ),
+                    );
+                  },
+                  visualDensity: const VisualDensity(
+                    horizontal: 3,
+                    vertical: 3,
+                  ),
+                );
+              },
             ),
-            title: Text(
-              locais[index]["nome"],
-              style: const TextStyle(
-                fontWeight: FontWeight.bold
+          ),
+          Expanded(
+            child: FlutterMap(
+              options: const MapOptions(
+                initialCenter: LatLng(40.631375, -8.659969),  // UA
+                initialZoom: 7,
+                // interactionOptions: InteractionOptions(flags: ~InteractiveFlag.doubleTapZoom)
               ),
-            ),
-            trailing: Text(
-              locais[index]["distance"],
-              style: TextStyle(
-                color: Colors.green.shade700,
-              ),
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(0),
-              side: BorderSide(
-                color: Colors.green.shade700,
-                width: .25,
-              ),
-            ),
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => PoiScreen(poiID: locais[index]["id"]),
+              children: [
+                TileLayer(
+                  urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
                 ),
-              );
-            },
-            visualDensity: const VisualDensity(
-              horizontal: 3,
-              vertical: 3,
-            ),
-          );
-        },
-      ),
+                MarkerLayer(markers: locais.map((local){
+                  return Marker(
+                    width: 80,
+                    height: 80,
+                    point: LatLng(local["coord"]["lat"], local["coord"]["lng"]),
+                    child:
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => PoiScreen(
+                                poiID: local["id"],
+                              ),
+                            ),
+                          );
+                        },
+                        child: Icon(
+                          Icons.location_pin,
+                          size: 40,
+                        color: local["visited"] == true
+                            ? Colors.deepOrangeAccent.shade100 // locais já visitados
+                            : Colors.deepOrange, // locais não visitados
+                        ),
+                      )
+                  );
+                }).toList())
+              ],
+            )
+          )
+        ],
+      )
     );
   }
 }
@@ -138,10 +205,8 @@ class PoiScreen extends StatelessWidget {
               ),
             ),
             Text(
-              poi["distance"],
               style: const TextStyle(
                 fontSize: 18,
-              ),    
             ),
             Text(
               poi["king"],
