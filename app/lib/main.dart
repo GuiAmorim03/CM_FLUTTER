b44ca4d1efebc08b1147beae3c155abd96c8423e
Guilherme Amorim <guilhermeamorim@ua.pt>
2024-10-01 19:00:17 +0100

    guardar foto e mostrar

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,10 @@
-import 'dart:ui';
+import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -126,7 +127,6 @@ class _HomePageState extends State<HomePage> {
                     );
                   },
                   visualDensity: const VisualDensity(
-                    horizontal: 3,
                     vertical: 3,
                   ),
                 );
@@ -192,9 +192,72 @@ class PoiScreen extends StatefulWidget {
 }
 
 class _PoiScreenState extends State<PoiScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    final poi = locais.firstWhere((element) => element["id"] == widget.poiID);
+    print(poi);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(poi["nome"]),
+        backgroundColor: Colors.green.shade700,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: 
+        Center(
+          child: Column(
+            children: <Widget>[
+              
+              if (poi.containsKey("imagePath") && poi["imagePath"] != null)
+                Image.file(
+                  File(poi["imagePath"]),
+                  width: 300,
+                  height: 600,
+                  fit: BoxFit.cover,
+                )
+              else
+                ElevatedButton(
+                  onPressed: () async {
+                    final imagePath = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CameraScreen(poiID: widget.poiID),
+                      ),
+                    );
+
+                    if (imagePath != null && imagePath.isNotEmpty) {
+                      setState(() {
+                        poi["imagePath"] = imagePath;
+                      });
+                    }
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.green.shade700),
+                  ),
+                  child: const Text('Usar Câmera'),
+                ),
+            ],
+          ),
+        ),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({super.key, required this.poiID});
+
+  final int poiID;
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
   CameraController? _controller;
   List<CameraDescription>? _cameras;
-  bool _isCameraActive = false; // Novo estado para controlar a exibição da câmera
+  XFile? _capturedImage;
 
   @override
   void initState() {
@@ -211,14 +274,23 @@ class _PoiScreenState extends State<PoiScreen> {
 
   Future<void> takePicture() async {
     if (_controller != null && _controller!.value.isInitialized) {
-      try {
-        final picture = await _controller!.takePicture();
-        print('Foto capturada: ${picture.path}');
-        // Aqui você pode navegar para outra tela ou mostrar a imagem capturada
-      } catch (e) {
-        print('Erro ao tirar foto: $e');
-      }
+      final picture = await _controller!.takePicture();
+      setState(() {
+        _capturedImage = picture;
+      });
+      await _controller?.dispose();
+      _controller = null;
     }
+  }
+
+  Future<String> _saveImage() async {
+
+    final Directory appDir = await getApplicationDocumentsDirectory();
+
+    final String newPath = '${appDir.path}/foto_${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+    final File newImage = await File(_capturedImage!.path).copy(newPath);
+    return newPath;
   }
 
   @override
@@ -229,70 +301,101 @@ class _PoiScreenState extends State<PoiScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final poi = locais.firstWhere((element) => element["id"] == widget.poiID);
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(poi["nome"]),
+        title: const Text('Câmera'),
         backgroundColor: Colors.green.shade700,
         foregroundColor: Colors.white,
-        centerTitle: true,
       ),
-      body: 
-        Stack(
-          children: [
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    poi["nome"],
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24,
-                    ),
+      body: _capturedImage == null
+          ? _buildCameraPreview()  
+          : _buildCapturedImage(),
+    );
+  }
+
+  // Câmara a funcionar
+  Widget _buildCameraPreview() {
+    if (_controller == null || !_controller!.value.isInitialized) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return Stack(
+      children: [
+        CameraPreview(_controller!),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.green.shade700,
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    await takePicture();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.green.shade700,
+                    backgroundColor: Colors.white,
                   ),
-                  const Text(
-                    "TO DO",
-                    style: TextStyle(
-                      fontSize: 18,
-                    ),
+                  child: const Icon(
+                    Icons.camera_alt
                   ),
-                  // Botão para ativar a câmera
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _isCameraActive = true; // Ativa a câmera quando o botão é pressionado
-                      });
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green.shade700),
-                    ),
-                    child: const Text('Usar Câmera'),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-            // Exibir a câmera se estiver ativa
-            if (_isCameraActive) 
-              Column(
-                children: [
-                  Expanded(
-                    child: CameraPreview(_controller!), // Exibe a visualização da câmera
-                  ),
-                  ElevatedButton(
-                    onPressed: () async {
-                      await takePicture(); // Chama a função para tirar a foto
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green.shade700),
-                    ),
-                    child: const Text('Tirar Foto'),
-                  ),
-                ],
-              ),
-          ],
+          ),
         ),
+      ]
+    );
+  }
+
+  // Foto Tirada
+  Widget _buildCapturedImage() {
+    return Stack(
+      children: [
+        Image.file(File(_capturedImage!.path)),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child:
+          Container(
+            color: Colors.green.shade700,
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _capturedImage = null;
+                      initCamera();
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.green.shade700,
+                    backgroundColor: Colors.white,
+                  ),
+                  child: const Text(
+                    "Repetir",
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    final imagePath = await _saveImage();
+                    Navigator.pop(context, imagePath);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.green.shade700,
+                    backgroundColor: Colors.white,
+                  ),
+                  child: const Text(
+                    "Guardar",
+                  ),
+                )
+              ],
+            ),
+          )
+        )
+      ]
     );
   }
 }
