342ff7907f6173a4746ae37d0e45c54f2ba91e9f
Guilherme Amorim <guilhermeamorim@ua.pt>
2024-10-05 17:36:48 +0100

    novo screen para o POI

--- app/lib/screens/poi.dart
+++ app/lib/screens/poi.dart
@@ -27,67 +27,109 @@ class _PoiScreenState extends State<PoiScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(poi["nome"]),
-        backgroundColor: Colors.green.shade700,
-        foregroundColor: Colors.white,
-        centerTitle: true,
-      ),
       body: 
-        Center(
+        Container(
+          width: double.infinity,
+          color: Colors.green.shade100,
           child: Column(
             children: <Widget>[
-              if (poi.containsKey("visited") && poi["visited"]["path"] != null)
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: poi["visited"]["path"].startsWith("photos")
-                    ? Image.asset(
-                      poi["visited"]["path"],
+                Row(
+                  children: <Widget> [
+                    Container(
                       width: screenWidth * 0.6,
                       height: screenHeight * 0.5,
-                      fit: BoxFit.cover,
-                    )
-                    : Image.file(
-                      File(poi["visited"]["path"]),
-                      width: screenWidth * 0.6,
-                      height: screenHeight * 0.5,
-                      fit: BoxFit.cover,
-                    ),
-                )
-              else
-                Row( 
-                  children: <Widget>[
-                    Image.asset(
-                      "photos/no-photo.jpg",
-                      width: screenWidth * 0.6,
-                      height: screenHeight * 0.5,
-                      fit: BoxFit.cover,
-                    ),      
-                    ElevatedButton(
-                      onPressed: () async {
-                        final imagePath = await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => CameraScreen(poiID: widget.poiID),
+                      padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                      margin: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.green.shade700,
+                            width: 5,
                           ),
-                        );
-                        if (imagePath != null && imagePath.isNotEmpty) {
-                          setState(() {
-                            poi["visited"] = {"path": imagePath, "date": DateTime.now().toIso8601String()};
-                          });
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green.shade700,
-                        padding: const EdgeInsets.all(10.0),
-                        alignment: Alignment.center,
-                      ),
-                      child: const Icon(
-                        Icons.camera_alt,
-                        color: Colors.white,
-                      ),
+                        ),
+                        child: poi.containsKey("visited") && poi["visited"]["path"] != null
+                          ? poi["visited"]["path"].startsWith("photos")
+                            ? Image.asset(
+                                poi["visited"]["path"],
+                                fit: BoxFit.cover,
+                            )
+                            : Image.file(
+                              File(poi["visited"]["path"]),
+                              fit: BoxFit.cover,
+                            )
+                          : Image.asset(
+                              "photos/no-photo.jpg",
+                              fit: BoxFit.cover,
+                          )
+                      )
                     ),
-                  ],
+                    Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            if (poi.containsKey("visited") && poi["visited"]["path"] != null) ...[
+                              Text(
+                                "Scanned on",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              Text(
+                                "${poi["visited"]["date"]}",
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ]
+                            else
+                            ElevatedButton(
+                              onPressed: () async {
+                                final imagePath = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => CameraScreen(poiID: widget.poiID),
+                                  ),
+                                );
+                                if (imagePath != null && imagePath.isNotEmpty) {
+                                  setState(() {
+                                    poi["visited"] = {"path": imagePath, "date": DateTime.now().toIso8601String().split('T')[0]};
+                                  });
+                                }
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.green.shade700,
+                                padding: const EdgeInsets.all(10.0),
+                                alignment: Alignment.center,
+                              ),
+                              child: const Icon(
+                                Icons.camera_alt,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                  ]
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          "Friends Activity",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "TO DO",
+                      )
+                    ]
+                  )
                 )
             ],
           ),
